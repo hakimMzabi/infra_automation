93b15d1674d693f665d21a847f60b9523607c152
hakim <hakim.mzabi@etu.u-pec.fr>
2020-11-23 21:00:35 +0100

    test oozie WF with a test script

---
+++ deploy_script.sh
@@ -0,0 +1,1 @@
+oozie job -oozie http://localhost:11000/oozie -config oozie/job.properties -submit
--- test/test.sh
+++ test/test.sh
@@ -8,5 +8,3 @@ hdfs dfs -mkdir project/data/
 
 hdfs dfs -mkdir project/automation_project
 hdfs dfs -put datagang/infra_automation/* project/automation_project/
-
-oozie job -oozie http://localhost:11000/oozie -config job.properties -submit
