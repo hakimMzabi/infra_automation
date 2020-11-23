0fb58bf714133e892ec1fd0131d40cc598f262ff
hakim <hakim.mzabi@etu.u-pec.fr>
2020-11-23 21:19:26 +0100

    test oozie WF with a test script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-oozie job -oozie http://localhost:11000/oozie -config oozie/job.properties -submit
+oozie job -oozie http://79663291-27f5-4fb7-ac28-634c79cec301.pub.instances.scw.cloud:11000/oozie -config oozie/job.properties -run
