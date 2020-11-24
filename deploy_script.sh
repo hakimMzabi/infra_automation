f40e66f98f562e35617a222b763c1ec75c088a39
hakim <hakim.mzabi@etu.u-pec.fr>
2020-11-25 00:00:25 +0100

    test oozie WF with a test script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+hdfs dfs -put datagang/infra_automation/* project/automation_project/
 oozie job -oozie http://d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:11000/oozie -config job.properties -submit
--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-hdfs dfs -mkdir lab
-hdfs dfs -mkdir lab/data/
-
-hdfs dfs -mkdir project
-hdfs dfs -mkdir project/data/
-
-hdfs dfs -mkdir project/automation_project
-hdfs dfs -put datagang/infra_automation/* project/automation_project/
+hdfs dfs rm -r test_oozie
+hdfs dfs -mkdir test_oozie
