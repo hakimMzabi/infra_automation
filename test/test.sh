cecd648113c01fbb22a1d7f5106aaecc5e299c7c
hakim <hakim.mzabi@etu.u-pec.fr>
2020-12-06 14:05:52 +0100

    test oozie WF with a test script

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hdfs dfs -mkdir /d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:8020/user/datagang/lab
+hdfs dfs -mkdir -p /d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:8020/user/datagang/lab
