292caf6afe0ad2c102d0a13ff261e613f45636bd
hakim <hakim.mzabi@etu.u-pec.fr>
2020-12-08 18:34:04 +0100

    test oozie WF with a test script

--- jobs/test_udf.py
+++ jobs/test_udf.py
@@ -1,28 +1,25 @@
-
-import pyspark
 from pyspark import SQLContext, SparkContext, SparkConf
 from pyspark.sql import *
 from pyspark.sql.functions import udf
-from pyspark.sql.types import *
+
 import pyspark.sql.functions as F
 from pyspark.sql.functions import *
-from math import *
 
-
-conf = pyspark.SparkConf()
-sc = pyspark.SparkContext.getOrCreate(conf)
+conf = SparkConf().setAppName('test')
+sc = SparkContext.getOrCreate(conf)
+sc.setLogLevel('ERROR')
 spark = SQLContext(sc)
 
 print("this is a test !")
-#columns = ["x", "y"]
-#data = [(5, 3), (8, 5), (5, 7), (3, 6)]
-#df = spark.createDataFrame(data).toDF(*columns)
-#df.show()
+# columns = ["x", "y"]
+# data = [(5, 3), (8, 5), (5, 7), (3, 6)]
+# df = spark.createDataFrame(data).toDF(*columns)
+# df.show()
 
 
-#def distance(x, y):
+# def distance(x, y):
 #    return sqrt(((x - y) ^ 2) + ((y + x) ^ 2))
 
 
-#distance_udf = udf(lambda a, b: distance(a, b), DoubleType())
-#df = df.select('x', 'y', distance_udf('x', 'y').alias('distance'))
+# distance_udf = udf(lambda a, b: distance(a, b), DoubleType())
+# df = df.select('x', 'y', distance_udf('x', 'y').alias('distance'))
