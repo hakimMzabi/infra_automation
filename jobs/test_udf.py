d9adf8d28bbb11ed39b9736fbc0fd042661992a0
hakim <hakim.mzabi@etu.u-pec.fr>
2020-12-08 18:01:50 +0100

    test oozie WF with a test script

--- jobs/test_udf.py
+++ jobs/test_udf.py
@@ -16,11 +16,12 @@ spark = SQLContext(sc)
 columns = ["x", "y"]
 data = [(5, 3), (8, 5), (5, 7), (3, 6)]
 df = spark.createDataFrame(data).toDF(*columns)
+df.show()
 
 
-def distance(x, y):
-    return sqrt(((x - y) ^ 2) + ((y + x) ^ 2))
+#def distance(x, y):
+#    return sqrt(((x - y) ^ 2) + ((y + x) ^ 2))
 
 
-distance_udf = udf(lambda a, b: distance(a, b), DoubleType())
-df = df.select('x', 'y', distance_udf('x', 'y').alias('distance'))
+#distance_udf = udf(lambda a, b: distance(a, b), DoubleType())
+#df = df.select('x', 'y', distance_udf('x', 'y').alias('distance'))
