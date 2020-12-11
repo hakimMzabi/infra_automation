2b946723eee77e6b65045c2f2309aded43235da4
hakim <hakim.mzabi@etu.u-pec.fr>
2020-12-11 17:13:39 +0100

    test oozie WF with a test script

---
+++ deploy_job.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+spark-submit --master yarn --queue default --conf spark.pyspark.python=/home/datagang/.conda/envs/datagang/bin/python --driver-memory=2g --executor-memory=2g --num-executors=2 $(dirname $0)/jobs/Extr_load_spotify.py
