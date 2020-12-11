0625b86a8331628e44ca13e059ff1bc8532db6a1
hakim <hakim.mzabi@etu.u-pec.fr>
2020-12-11 15:38:09 +0100

    test oozie WF with a test script

--- jobs/Extr_load_spotify.py
+++ jobs/Extr_load_spotify.py
@@ -11,7 +11,7 @@ spark = SparkSession.builder \
     .appName('SparkByExamples.com') \
     .getOrCreate()
 sc = spark.sparkContext
-spotify_data_lake = "hdfs://d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:8020/user/datagang/lab/spotify"
+spotify_data_lake = "hdfs://d271ee89-3c06-4d40-b9d6-d3c1d65feb57.priv.instances.scw.cloud:8020/user/datagang/lake/spotify"
 
 
 def main():
